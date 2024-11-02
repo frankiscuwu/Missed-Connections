3d579aed3789634f1cbfe039911da2ee7cdc6227
Joseph Marotta <marottaj@bu.edu>
2024-11-02 11:53:17 -0400

    🧑‍🔬 Updated Flask Code with correct return

--- MFJJ/backend/main.py
+++ MFJJ/backend/main.py
@@ -2,19 +2,24 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/locate', methods=['GET', 'POST'])
 def locate():
     if request.method == "POST":
         if request.is_json:
             data = request.get_json()
 
+            # debug
+            print(data)
+
             # send to backend
 
+            return jsonify(data)
+
+    elif request.method == "GET":
+        return 'Hi frank'
     else:
         return jsonify('{Error}')
 
-
-    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True, host="10.239.101.11")
