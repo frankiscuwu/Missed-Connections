950e817ba7ea3bb1576aff07dca3691ac0846ae5
mhit30 <mohammedhit30@gmail.com>
2024-11-02 14:31:57 -0400

    added views functions😛

--- MFJJ/django-server/backend2/mfjj/views.py
+++ MFJJ/django-server/backend2/mfjj/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Location
+import json
+
 
 # Create your views here.
+def post_location(request):
+    if request.method == "POST":
+        data = json.loads(request.body) 
+        user_id = data.get("user_id")
+        latitude = data.get("latitude")
+        longitude = data.get("longitude")
+
+        location = Location(user_id=user_id, latitude = latitude, longitutde=longitude)
+        location.save()
+        return JsonResponse({"message": "Location added succesfully!"}, status=201)
+    return JsonResponse({"error": "Invalid request method"}, status=400)
+
+def get_users(request):
+    if request.method == "GET":
+        
