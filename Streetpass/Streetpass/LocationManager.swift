6cbd64329ebc1728cbf574331b3178088651f361
Frank Yang <frankyang009@gmail.com>
2024-11-02 19:29:28 -0400

    circular

--- Streetpass/Streetpass/LocationManager.swift
+++ Streetpass/Streetpass/LocationManager.swift
@@ -1,24 +1,24 @@
 import CoreLocation
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    private var locationManager: CLLocationManager
+    private var locationManager = CLLocationManager()
     @Published var currentLocation: CLLocation?
-
+    
+    // Callback closure for location updates
+    var onLocationUpdate: ((CLLocation) -> Void)?
+    
     override init() {
-        locationManager = CLLocationManager()
         super.init()
         locationManager.delegate = self
-        locationManager.requestAlwaysAuthorization() // Request always authorization for background location updates
-        locationManager.startUpdatingLocation()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startMonitoringSignificantLocationChanges()
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         currentLocation = location
-    }
-    
-    func startBackgroundLocationUpdates() {
-        locationManager.allowsBackgroundLocationUpdates = true
-        locationManager.startUpdatingLocation()
+        
+        // Call the closure to send location data
+        onLocationUpdate?(location)
     }
 }
