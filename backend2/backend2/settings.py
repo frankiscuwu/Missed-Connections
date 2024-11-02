0ba995ecb4f8f77a3059ca35d7f6580e12a38aed
Joseph Marotta <marottaj@bu.edu>
2024-11-02 14:59:07 -0400

    😢 Updated file structure

--- backend2/backend2/settings.py
+++ backend2/backend2/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'backend2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'LVGZNTsafcfvPASxHgQZbRRMBaXVrKtM',
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '17561',
     }
 }
+
 
 
 # Password validation
