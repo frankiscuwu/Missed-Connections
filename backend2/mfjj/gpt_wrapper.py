f5935f8ffba0461cf890f68a6d685a35d04efee4
Joseph Marotta <marottaj@bu.edu>
2024-11-02 19:13:02 -0400

    🥶Fixed ChatGPT

--- backend2/mfjj/gpt_wrapper.py
+++ backend2/mfjj/gpt_wrapper.py
@@ -9,7 +9,7 @@ def call_gpt(content):
         messages=[
             {
                 "role": "user",
-                "content": "Hello ChatGPT, the first person in this list is me. Then there is a list of other people and their profiles. Tell me which three of these people could good friends with me, based on our interests. Always find at least one match and be optimistic. Only give up to at most 3 people to match with. Give back JSON with the at most three usernames (if no matches put None), and then put 'content' with your analysis. Begin data: \n" + str(content),
+                "content": "Hello ChatGPT, the first person in this list is me. Then there is a list of other people and their profiles. Tell me which three of these people could be good friends with me, based on our interests. Remember that I am the first person in the list. Do not include me as a match. Always find at least one match and be optimistic. Only give up to at most 3 people to match with. Give back JSON with the at most three usernames (if no matches put None), and then put 'content' with your analysis. Begin data: \n" + str(content),
             }
         ],
         model="gpt-4o-mini-2024-07-18",
