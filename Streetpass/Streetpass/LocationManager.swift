05df28832cbe1e1933dba76b20f75322cb410ba2
Frank Yang <frankyang009@gmail.com>
2024-11-02 18:03:11 -0400

    fixed stupid

--- Streetpass/Streetpass/LocationManager.swift
+++ Streetpass/Streetpass/LocationManager.swift
@@ -1,6 +1,8 @@
+import Foundation
 import CoreLocation
+import Combine
 
-class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     private var locationManager: CLLocationManager
     @Published var currentLocation: CLLocation?
 
@@ -8,32 +10,17 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         locationManager = CLLocationManager()
         super.init()
         locationManager.delegate = self
-        
-        // Request location permissions
-        locationManager.requestAlwaysAuthorization() // or requestWhenInUseAuthorization()
-        locationManager.allowsBackgroundLocationUpdates = true
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest // Set desired accuracy
-        locationManager.startUpdatingLocation() // Start receiving location updates
-    }
-
-    func startUpdatingLocation() {
-        // Check if location services are enabled
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.startUpdatingLocation()
-        } else {
-            print("Location services are not enabled")
-        }
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         currentLocation = location
-        print("Updated location: \(location.coordinate.latitude), \(location.coordinate.longitude)")
-        // Handle your location update logic here (e.g., sending it to a server)
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to find user's location: \(error.localizedDescription)")
-        currentLocation = nil // Set currentLocation to nil on failure
+        currentLocation = nil
     }
 }
