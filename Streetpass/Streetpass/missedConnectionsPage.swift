3982c2396f3741efa04a12f0b4cfdcd12ee41195
Frank Yang <frankyang009@gmail.com>
2024-11-02 21:22:04 -0400

    idk

--- Streetpass/Streetpass/missedConnectionsPage.swift
+++ Streetpass/Streetpass/missedConnectionsPage.swift
@@ -2,18 +2,14 @@
 //  missedConnectionsPage.swift
 //  Streetpass
 //
-//  Created by Jodi Yu on 11/2/24.
+//  Created by Frank Yang on 11/2/24.
 //
 import SwiftUI
 
 struct missedConnectionsPage: View {
     var body: some View {
         VStack {
-            Text("Missed Connections Page")
-                .font(.largeTitle)
-                .padding()
-            Text("This page will display missed connections.")
-                .padding()
+
         }
     }
 }
